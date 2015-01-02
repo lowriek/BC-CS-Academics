67b154eee221a6350bf82c7149f92048aaf7119d
lowriek <lowriek@bc.edu>
2015-01-02 09:38:13 -0500

    Added 2007 Honors Theses

---
+++ thesesbyyear/thesis2007.js
@@ -0,0 +1,26 @@
+document.write('\
+<div class="well">\
+	<h3>2007 Honors Theses</h3>				\
+	<div class="q">	\
+		<h3 class="qhead"><a href="#q200701">Web 2.0 and Communication in Nonprofit Organizations: A Case Study and Proof of Concept</a></h3>\
+		<div class="answer" id="q200701">\
+			<p><b>Author:</b> William Clerico</p>\
+			<p><b>Title:</b> <a href="pdf/07BillClerico.pdf"><em> Web 2.0 and Communication in Nonprofit Organizations: A Case Study and Proof of Concept</em></a></p>\
+			<p><b>Advisor:</b> William Ames</p>\
+			<p><b>Abstract</b></p>\
+			<p><font size="4">The nonprofit sector is a large piece of the U.S. economy with tremendous growth. There are 1.5 million different organizations with revenues of (670 billion annually. 109 million Americans volunteer each year. With such large amounts of money flowing through these organizations each year, there has been several studies looking at the efficiencies with which they operate.</font></p>\
+			<p><font size="4">In 2003, the Harvard Business Review published a comprehensive study of the nonprofit sector, specifically the largest 200,000 organizations which have revenue of more than $25,000 every year. They identified five major areas for improvement:\
+				<ol>\
+ 					<li>Reduce funding costs</li>\
+ 					<li>Distribute holdings faster</li>\
+ 					<li>Reduce program service costs</li>\
+ 					<li>Trim administrative costs</li>\
+ 					<li>Improve sector effectiveness</li>\
+				</ol>  \
+			</font></p>\
+			<p><font size="4">The authors of the article made several suggestions about how to improve these various areas, even brushing upon the topic of technology by way of online solicitation donation. However, they also mentioned the difficulties of improving efficiency without significant time and capital investment. It is also difficult to talk in the general sense about the application of technology when the scope of nonprofit organizations is so broad and so varied.</font></p>\
+			<p><font size="4">Click <a href="attachments/07ClericoThesisCode.zip">HERE</a> to download Bill Clerico\'s code.</font></p>						\
+		</div>   <!-- end answer -->\
+	</div><!-- end question -->	\
+</div>\
+');
